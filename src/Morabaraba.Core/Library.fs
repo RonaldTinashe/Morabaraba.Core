3ed49b95a38e6320501868f9a686acbb5d5d4d14
Ronald Munodawafa <ronaldmunodawafa@gmail.com>
2021-04-16 07:07:27 +0200

    Dark player issued 12 cows

--- src/Morabaraba.Core/Library.fs
+++ src/Morabaraba.Core/Library.fs
@@ -5,7 +5,12 @@ type Board<'J, 'O>
     and 'O : comparison = 
     Occupants of Map<'J, 'O>
 
+type Shade = Dark
+
+type Player = { Shade : Shade; Cows : int }
+
 let initial<'J, 'O> = 
     {| 
         Board = Occupants Map.empty
+        DarkPlayer = { Shade = Dark; Cows = 12 }
     |}
--- tests/Morabaraba.Core.Tests/Initialisation.fs
+++ tests/Morabaraba.Core.Tests/Initialisation.fs
@@ -14,5 +14,12 @@ let tests =
           let expected = Occupants Map.empty
           let actual = initial.Board
           Expect.equal actual expected "Board should be empty")
+
+      testCase 
+        "Game issues dark player with 12 cows"
+        (fun () -> 
+          let expected = { Shade = Dark; Cows = 12; }
+          let actual = initial.DarkPlayer
+          Expect.equal actual expected "Dark player should have 12 cows")
     ]
     
