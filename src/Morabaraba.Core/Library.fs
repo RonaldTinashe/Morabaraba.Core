c96cb3bb0e155cdf4208f820f72a5bc7dd84330a
Ronald Munodawafa <ronaldmunodawafa@gmail.com>
2021-04-16 08:22:21 +0200

    Add top-level comments

--- src/Morabaraba.Core/Library.fs
+++ src/Morabaraba.Core/Library.fs
@@ -1,15 +1,22 @@
-﻿module Morabaraba.Core
+﻿/// Logical kernel of the Morabaraba game
+module Morabaraba.Core
 
+/// Models the shade of the game
 type Shade = Dark | Light
 
+/// Models a player object
 type Player = { Shade : Shade; Cows : int }
 
+/// Models a non-shot move
 type MainMove = Placement of int
 
+/// Models a game's move
 type Move = { Main : MainMove; Shot : int option }
 
+/// Models a single historical event
 type Event = { Occupations: Map<int, Shade> }
 
+/// The game object's initial state
 let initial =
     let issue shade = { Shade = shade; Cows = 12 }
     {| 
@@ -19,6 +26,7 @@ let initial =
         History = ([] : Event list)
     |}
 
+/// Applies move with historical context
 let play { Main = mainMove } history =
     match mainMove with
     | Placement junction -> 
