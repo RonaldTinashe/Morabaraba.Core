fb56bf42faef336447951c5a14c29f708efc912d
Ronald Munodawafa <ronaldmunodawafa@gmail.com>
2021-04-16 08:48:00 +0200

    Use occupy function

--- src/Morabaraba.Core/Library.fs
+++ src/Morabaraba.Core/Library.fs
@@ -28,8 +28,9 @@ let initial =
 
 /// Applies move with historical context
 let play { Main = mainMove } history =
+    let occupy junction shade occupations = Map.add junction shade occupations
     match mainMove, history with
-    | Placement junction, [] -> Some [{ Occupations = Map.add junction Dark Map.empty; Player = { Shade = Dark; Cows = 11 } }]
+    | Placement junction, [] -> Some [{ Occupations = occupy junction Dark Map.empty; Player = { Shade = Dark; Cows = 11 } }]
     | Placement junction, [{ Occupations = occupations; Player = { Shade = Dark }}] ->
-        { Occupations = Map.add junction Light occupations; Player = { Shade = Light; Cows = 11 } } :: history |> Some
+        { Occupations = occupy junction Light occupations; Player = { Shade = Light; Cows = 11 } } :: history |> Some
     | _ -> None
