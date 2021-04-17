3e9f2487b8262a66f40a812c19aec501c5332bbe
Ronald Munodawafa <ronaldmunodawafa@gmail.com>
2021-04-17 15:09:51 +0200

    Fix formatting and add commets

--- src/Morabaraba.Core/Playing.fs
+++ src/Morabaraba.Core/Playing.fs
@@ -4,8 +4,24 @@ module Morabaraba.Core.Playing
 let play { Main = mainMove } history =
     let occupy junction shade occupations = Map.add junction shade occupations
     match mainMove, history with
-    | Placement junction, [] -> Some [{ Occupations = occupy junction Dark Map.empty; Player = { Shade = Dark; Cows = 11 } }]
-    | Placement junction, [{ Occupations = occupations; Player = { Shade = Dark }}] ->
-        { Occupations = occupy junction Light occupations; Player = { Shade = Light; Cows = 11 } } :: history |> Some
+    // First move
+    | Placement junction, [] ->
+        let history =
+            [
+                {
+                    Occupations = occupy junction Dark Map.empty
+                    Player = { Shade = Dark; Cows = 11 }
+                }
+            ]
+        Some history
+    // Light player's turn
+    | Placement junction, 
+        [{ Occupations = occupations; Player = { Shade = Dark }}] ->
+        let history = 
+            {
+                Occupations = occupy junction Light occupations
+                Player = { Shade = Light; Cows = 11 }
+            } :: history
+        Some history
     | _ -> None
     
