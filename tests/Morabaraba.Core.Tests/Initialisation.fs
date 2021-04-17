d3cc2cd038e3603e415c106923ed5a8603a3db48
Ronald Munodawafa <ronaldmunodawafa@gmail.com>
2021-04-17 15:09:17 +0200

    Fix formatting

--- tests/Morabaraba.Core.Tests/Initialisation.fs
+++ tests/Morabaraba.Core.Tests/Initialisation.fs
@@ -6,35 +6,39 @@ open Morabaraba.Core.Initialisation
 
 [<Tests>]
 let tests =
-  testList 
-    "Initialisation"
-    [
-      testCase 
-        "Game initialises with empty board"
-        (fun () -> 
-          let expected = Map.empty
-          let actual = initial.Occupations
-          Expect.equal actual expected "Board should be empty")
+    testList 
+        "Initialisation"
+        [   
+            testCase 
+                "Game initialises with empty board"
+                (fun () -> 
+                    let expected = Map.empty
+                    let actual = initial.Occupations
+                    let message = "Board should be empty"
+                    Expect.equal actual expected message)
 
-      testCase 
-        "Game issues dark player with 12 cows"
-        (fun () -> 
-          let expected = { Shade = Dark; Cows = 12; }
-          let actual = initial.DarkPlayer
-          Expect.equal actual expected "Dark player should have 12 cows")
+            testCase 
+                "Game issues dark player with 12 cows"
+                (fun () -> 
+                    let expected = { Shade = Dark; Cows = 12; }
+                    let actual = initial.DarkPlayer
+                    let message = "Dark player should have 12 cows"
+                    Expect.equal actual expected message)
 
-      testCase 
-        "Game issues light player with 12 cows"
-        (fun () -> 
-          let expected = { Shade = Light; Cows = 12; }
-          let actual = initial.LightPlayer
-          Expect.equal actual expected "Light player should have 12 cows")
+            testCase 
+                "Game issues light player with 12 cows"
+                (fun () -> 
+                    let expected = { Shade = Light; Cows = 12; }
+                    let actual = initial.LightPlayer
+                    let message = "Light player should have 12 cows"
+                    Expect.equal actual expected message)
 
-      testCase 
-        "Game starts with empty history"
-        (fun () -> 
-          let expected = []
-          let actual = initial.History
-          Expect.equal actual expected "History should be empty")
-    ]
+            testCase 
+                "Game starts with empty history"
+                (fun () -> 
+                    let expected = []
+                    let actual = initial.History
+                    let message = "History should be empty"
+                    Expect.equal actual expected message)
+        ]
     
--- tests/Morabaraba.Core.Tests/Playing.fs
+++ tests/Morabaraba.Core.Tests/Playing.fs
@@ -8,28 +8,34 @@ open Morabaraba.Core.Playing
 let ``placement on board`` =
     testList 
         "Placing on an empty board"
-        [
+        [   
             testCase
                 "Placement by dark player on empty board's 4th junction"
                 (fun () -> 
-                  let expected = Some Dark
-                  let actual =
-                    play { Main = Placement 4; Shot = None } [] |> 
+                    let expected = Some Dark
+                    let actual =
+                        let move = { Main = Placement 4; Shot = None }
+                        let history = []
+                        play move history |> 
                         let eventBinder history = List.tryHead history
-                        let occupationBinder event = Map.tryFind 4 event.Occupations
+                        let occupationBinder event = 
+                            Map.tryFind 4 event.Occupations
                         Option.bind eventBinder >> Option.bind occupationBinder
-                  Expect.equal actual expected "Cow should be placed")
+                    Expect.equal actual expected "Cow should be placed")
 
             testCase
                 "Placement by dark player on empty board's 1st junction"
                 (fun () -> 
-                  let expected = Some Dark
-                  let actual =
-                    play { Main = Placement 1; Shot = None } [] |> 
+                    let expected = Some Dark
+                    let actual =
+                        let move = { Main = Placement 1; Shot = None }
+                        let history = []
+                        play move history |> 
                         let eventBinder history = List.tryHead history
-                        let occupationBinder event = Map.tryFind 1 event.Occupations
+                        let occupationBinder event = 
+                            Map.tryFind 1 event.Occupations
                         Option.bind eventBinder >> Option.bind occupationBinder
-                  Expect.equal actual expected "Cow should be placed")
+                    Expect.equal actual expected "Cow should be placed")
         ]
     
 [<Tests>]
@@ -40,12 +46,17 @@ let ``placement by light player`` =
             testCase
                 "Placement by light player on a board's 1st junction"
                 (fun () -> 
-                  let expected = Some Light
-                  let history = [ { Occupations = Map.add 10 Dark Map.empty; Player = { Shade = Dark; Cows = 11} }]
-                  let actual =
-                    play { Main = Placement 1; Shot = None } history |> 
+                    let expected = Some Light
+                    let history = 
+                        [ { Occupations = Map.add 10 Dark Map.empty
+                            Player = { Shade = Dark; Cows = 11 } } ]
+                    let actual =
+                        let move = { Main = Placement 1; Shot = None }
+                        play move history |> 
                         let eventBinder history = List.tryHead history
-                        let occupationBinder event = Map.tryFind 1 event.Occupations
-                        Option.bind eventBinder >> Option.bind occupationBinder
-                  Expect.equal actual expected "Cow should be placed")
+                        let occupationBinder event = 
+                          Map.tryFind 1 event.Occupations
+                        Option.bind eventBinder >> 
+                        Option.bind occupationBinder
+                    Expect.equal actual expected "Cow should be placed")
         ]
